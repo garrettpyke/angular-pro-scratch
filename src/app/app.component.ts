9e04cc4c63ae8afc74f25788bd887bbc092d719d
Garrett <garrettpyke@msn.com>
2023-02-09 14:29:17 -0700

    Initialize new project, create initial components, modules & dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { User } from './auth-form/auth-form.interface';
+
 @Component({
   selector: 'app-root',
-  template: ` <div>Hi!</div> `,
+  template: `
+    <div>
+      <auth-form (submitted)="createUser($event)">
+        <h3>Create account</h3>
+      </auth-form>
+      <auth-form (submitted)="loginUser($event)">
+        <h3>Login</h3>
+      </auth-form>
+    </div>
+  `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-pro-gp';
+
+  createUser(user: User) {
+    console.log('Create account', user);
+  }
+
+  loginUser(user: User) {
+    console.log('Login', user);
+  }
 }
