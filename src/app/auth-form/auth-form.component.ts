d858e6f29281b9198356ee37fdd361f26c110f39
Garrett <garrettpyke@msn.com>
2023-02-10 19:05:20 -0700

    Use native renderer

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -3,12 +3,15 @@ import {
   DoCheck,
   Output,
   ViewChildren,
+  ViewChild,
+  ElementRef,
   AfterViewInit,
   EventEmitter,
   QueryList,
   ContentChildren,
   AfterContentInit,
   ChangeDetectorRef,
+  Renderer2,
 } from '@angular/core';
 
 import { AuthRememberComponent } from './auth-remember.component';
@@ -25,7 +28,7 @@ import { User } from './auth-form.interface';
         <!-- This is where the h3 in app.component is projected in -->
         <label>
           Email address
-          <input type="email" name="email" ngModel />
+          <input type="email" name="email" ngModel #email />
         </label>
         <label>
           Password
@@ -34,29 +37,35 @@ import { User } from './auth-form.interface';
         <ng-content select="auth-remember"></ng-content>
         <auth-message [style.display]="showMessage ? 'inherit' : 'none'">
         </auth-message>
-        <auth-message [style.display]="showMessage ? 'inherit' : 'none'">
-        </auth-message>
         <ng-content select="button"></ng-content>
       </form>
     </div>
   `,
-  styles: [``],
+  styles: [
+    `
+      .email {
+        border-color: #9f72e6;
+      }
+    `,
+  ],
 })
 export class AuthFormComponent
   implements DoCheck, AfterContentInit, AfterViewInit
 {
-  constructor(private cd: ChangeDetectorRef) {}
-
-  showMessage: boolean;
-
   // a view child queries the view we're currently inside
   @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent>;
+
+  @ViewChild('email') email: ElementRef; // ElementRef allows you to use nativeElement methods
 
   // queries the projected ngContent item(s) (outside)
   @ContentChildren(AuthRememberComponent)
   remember: QueryList<AuthRememberComponent>;
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
+
+  showMessage: boolean;
+
+  constructor(private renderer: Renderer2, private cd: ChangeDetectorRef) {}
 
   ngDoCheck(): void {
     //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
@@ -76,6 +85,23 @@ export class AuthFormComponent
   }
 
   ngAfterViewInit() {
+    //* nativeElement api allows all of the DOM methods
+    // this.email.nativeElement.setAttribute(
+    //   'placeholder',
+    //   'Enter your email address'
+    // );
+    // this.email.nativeElement.classList.add('email');
+    // this.email.nativeElement.focus();
+
+    //* Renderer allows intercept of render methods, can render outside of web DOM (platform agnostic)
+    this.renderer.setAttribute(
+      this.email.nativeElement,
+      'placeholder',
+      'Enter your email address'
+    );
+    this.renderer.addClass(this.email.nativeElement, 'email');
+    this.renderer.selectRootElement(this.email.nativeElement, true).focus();
+
     if (this.message) {
       this.message.forEach((message) => {
         message.days = 30;
